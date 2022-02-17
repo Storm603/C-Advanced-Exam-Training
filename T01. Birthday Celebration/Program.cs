204341b4b358ce06fcf62d043a5f7a8047bd866a
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-02-17 19:49:00 +0200

    Add files via upload

---
+++ T01. Birthday Celebration/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace T01._Birthday_Celebration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> guests = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> plates = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            int wastedFood = 0;
+
+            int guest = guests.Peek();
+            int plate = 0;
+
+            while (guests.Count != 0 && plates.Count != 0)
+            {
+                plate = plates.Peek();
+
+                if (guest > plate)
+                {
+                    guest -= plate;
+                }
+                else if (guest < plate)
+                {
+                    wastedFood += plate - guest;
+
+                    guests.Dequeue();
+                    if (guests.Count > 0)
+                    {
+                        guest = guests.Peek();
+                    }
+                }
+                else
+                {
+                    guests.Dequeue();
+                    if (guests.Count > 0)
+                    {
+                        guest = guests.Peek();
+                    }
+                }
+                plates.Pop();
+
+            }
+
+            if (guests.Count > 0)
+            {
+                guests.Dequeue();
+                List<int> vict = new List<int>(guests);
+                vict.Insert(0, guest);
+                Console.WriteLine($"Guests: {string.Join(" ", vict)}");
+            }
+            else if (plates.Count > 0)
+            {
+                //plates.Pop();
+                List<int> vict = new List<int>(plates);
+                //vict.Insert(0, plate);
+                Console.WriteLine($"Plates: {string.Join(" ", vict)}");
+            }
+
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
---
+++ T02. The Battle of The Five Armies/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data;
+
+namespace T02._The_Battle_of_The_Five_Armies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int armor = int.Parse(Console.ReadLine());
+
+            int dimentions = int.Parse(Console.ReadLine());
+
+            int r = 0;
+            int c = 0;
+
+            char[] arr = Console.ReadLine().ToCharArray();
+            char[,] world = new Char[dimentions, arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                world[0, i] = arr[i];
+            }
+
+            for (int i = 1; i < dimentions; i++)
+            {
+                arr = Console.ReadLine().ToCharArray();
+
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] == 'A')
+                    {
+                        r = i;
+                        c = j;
+                    }
+                    world[i, j] = arr[j];
+                }
+                
+            }
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = input[0];
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+
+                world[row, col] = 'O';
+                world[r, c] = '-';
+
+                armor--;
+
+                if (command == "up")
+                {
+                    r--;
+                    if (!(r < world.GetLength(0) && r >= 0))
+                    {
+                        r++;
+                    }
+                }
+                else if (command == "down")
+                {
+                    r++;
+                    if (!(r < world.GetLength(0) && r >= 0))
+                    {
+                        r--;
+                    }
+                }
+                else if (command == "left")
+                {
+                    c--;
+                    if (!(c < world.GetLength(1) && c >= 0))
+                    {
+                        c++;
+                    }
+
+                }
+                else if (command == "right")
+                {
+                    c++;
+                    if (!(c < world.GetLength(1) && c >= 0))
+                    {
+                        c++;
+                    }
+                }
+
+                
+                if (world[r, c] == 'O')
+                {
+                    armor -= 2;
+                }
+
+                if (armor <= 0)
+                {
+                    Console.WriteLine($"The army was defeated at {r};{c}.");
+                    world[r, c] = 'X';
+                    break;
+                }
+
+                if (world[r,c] == 'M')
+                {
+                    Console.WriteLine($"The army managed to free the Middle World! Armor left: {armor}");
+                    world[r, c] = '-';
+                    break;
+                }
+
+                world[r, c] = 'A';
+            }
+
+            for (int i = 0; i < world.GetLength(0); i++)
+            {
+                for (int j = 0; j < world.GetLength(1); j++)
+                {
+                    Console.Write(world[i,j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
