2e41152b809cd6746dfaf55ddbd161559a757089
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-02-17 20:00:11 +0200

    Add files via upload

---
+++ T03. Drones/Airfield.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Drones
+{
+    public class Airfield
+    {
+        public List<Drone> Drones { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public double LandingStrip { get; set; }
+        public int Count => Drones.Count;
+
+        public Airfield(string name, int capacity, double landingStrip)
+        {
+            Name = name;
+            Capacity = capacity;
+            LandingStrip = landingStrip;
+            Drones = new List<Drone>();
+
+        }
+
+        public string AddDrone(Drone drone)
+        {
+            if (Count > Capacity)
+            {
+                return "Airfield is full.";
+            }
+
+            if (string.IsNullOrEmpty(drone.Name) || string.IsNullOrEmpty(drone.Brand) || drone.Range < 5 || drone.Range > 15)
+            {
+                return "Invalid drone";
+            }
+
+            Drones.Add(drone);
+            return $"Successfully added {drone.Name} to the airfield.";
+        }
+
+        public bool RemoveDrone(string name)
+        {
+            foreach (Drone drone in Drones)
+            {
+                if (drone.Name == name)
+                {
+                    Drones.Remove(drone);
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public int RemoveDroneByBrand(string brand)
+        {
+            int count = 0;
+
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                if (Drones[i].Brand == brand)
+                {
+                    count++;
+                    Drones.RemoveAt(i);
+                }
+            }
+
+            return count;
+        }
+
+        public Drone FlyDrone(string name)
+        {
+            foreach (Drone drone in Drones)
+            {
+                if (drone.Name == name)
+                {
+                    drone.Available = false;
+                    return drone;
+                }
+            }
+
+            return null;
+        }
+
+        public List<Drone> FlyDronesByRange(int range)
+        {
+            List<Drone> dronesFlying = new List<Drone>();
+
+            foreach (Drone drone in Drones)
+            {
+                if (drone.Range >= range)
+                {
+                    drone.Available = false;
+                    dronesFlying.Add(drone);
+                }
+            }
+
+            return dronesFlying;
+        }
+
+        public StringBuilder Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine($"Drones available at {Name}");
+            foreach (Drone drone in Drones)
+            {
+                if (drone.Available)
+                {
+                    sb.AppendLine(drone.ToString());
+                }
+            }
+
+            return sb;
+        }
+    }
+}
---
+++ T03. Drones/Drone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drones
+{
+    public class Drone
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Range { get; set; }
+        public bool Available { get; set; }
+
+        public Drone(string name, string brand, int range)
+        {
+            Name = name;
+            Brand = brand;
+            Range = range;
+            Available = true;
+        }
+
+        public override string ToString()
+        {
+            return $"Drone: {Name}\nManufactured by: {Brand}\nRange: {Range} kilometers";
+        }
+    }
+}
