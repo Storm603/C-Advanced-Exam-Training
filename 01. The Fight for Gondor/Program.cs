c5cdc4b7e6c654e65aa54b1d5f713c5f19e1017f
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-02-17 19:47:27 +0200

    Add files via upload

---
+++ 01. The Fight for Gondor/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._The_Fight_for_Gondor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waves = int.Parse(Console.ReadLine());
+
+            Queue<int> plates = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            
+
+            //int plateHP = plates.Dequeue();
+
+            for (int wave = 1; wave <= waves; wave++)
+            { 
+                Stack<int> warrior = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+                if (wave % 3 == 0) // building defenses
+                {
+                    plates.Enqueue(int.Parse(Console.ReadLine()));
+                }
+
+                //int warriorHP = warrior.Pop();
+
+                while (true)
+                {
+                    if (warrior.Peek() > plates.Peek())
+                    {
+                        warrior.Push(warrior.Pop() - plates.Dequeue());
+                    }
+                    else if (warrior.Peek() < plates.Peek())
+                    {
+                        plateHP -= warriorHP;
+
+                        if (warrior.Count() == 0)
+                        {
+                            break;
+                        }
+
+                        warriorHP = warrior.Pop();
+                    }
+                    else
+                    {
+                        CheckEnd(warrior, plates, warriorHP);
+
+                        if (warrior.Count() == 0)
+                        {
+                            plateHP = plates.Dequeue();
+                            break;
+                        }
+
+                        plateHP = plates.Dequeue();
+                        warriorHP = warrior.Pop();
+                    }
+                }
+            }
+
+            Console.WriteLine("The people successfully repulsed the orc's attack.");
+            if (!plates.Any())
+            {
+                Console.WriteLine($"Plates left: {plateHP}");
+            }
+            else
+            {
+                Console.WriteLine($"Plates left: {plateHP}, {string.Join(", ", plates)}");
+            }
+        }
+
+        public static void CheckEnd(Stack<int> warrior, Queue<int> plates, int warriorHp)
+        {
+            if (plates.Count == 0)
+            {
+                
+                Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
+                if (!warrior.Any())
+                {
+                    Console.WriteLine($"Orcs left: {warriorHp}");
+                }
+                else
+                {
+                    Console.WriteLine($"Orcs left: {warriorHp}, {string.Join(", ", warrior)}");
+                }
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ 02. Super Maria/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+namespace _02._Super_Maria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lives = int.Parse(Console.ReadLine());
+
+            int dimentions = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new Char[dimentions, dimentions];
+
+            int r = 0;
+            int c = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (input[j] == 'M')
+                    {
+                        r = i;
+                        c = j;
+                    }
+
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string movement = input[0];
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+
+                matrix[row, col] = 'B';
+
+                matrix[r, c] = '-';
+
+                switch (movement)
+                {
+                    case "W":
+                        if (r - 1 >= 0)
+                        {
+                            r--;
+                        }
+                        break;
+                    case "A":
+                        if (c - 1 >= 0)
+                        {
+                            c--;
+                        }
+                        break;
+                    case "S":
+                        if (r + 1 < matrix.GetLength(1))
+                        {
+                            r++;
+                        }
+                        break;
+                    case "D":
+                        if (c + 1 < matrix.GetLength(0))
+                        {
+                            c++;
+                        }
+                        break;
+                }
+
+                lives--;
+
+                
+                if (matrix[r, c] == 'B')
+                {
+                    lives -= 2;
+                }
+                else if (matrix[r, c] == 'P')
+                {
+                    matrix[r, c] = '-';
+                    Console.WriteLine($"Mario has successfully saved the princess! Lives left: {lives}");
+                    break;
+                }
+
+                if (lives <= 0)
+                {
+                    Console.WriteLine($"Mario died at {r};{c}.");
+                    matrix[r, c] = 'X';
+                    break;
+                }
+
+                matrix[r, c] = 'M';
+
+            }
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
