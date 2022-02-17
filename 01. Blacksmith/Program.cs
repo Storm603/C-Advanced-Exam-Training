73d0169cd707542deecbf3d65954ab1ec2942084
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-02-17 19:52:18 +0200

    Add files via upload

---
+++ 01. Blacksmith/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+
+namespace _01._Blacksmith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> steel = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> carbon = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            Dictionary<string, int> forgedSwords = new Dictionary<string, int>();
+
+            while (steel.Count > 0 && carbon.Count > 0)
+            {
+                int value = steel.Peek() + carbon.Peek();
+
+                if (value == 70)
+                {
+                    steel.Dequeue();
+                    carbon.Pop();
+                    if (!forgedSwords.ContainsKey("Gladius"))
+                    {
+                        forgedSwords.Add("Gladius", 0);
+                    }
+
+                    forgedSwords["Gladius"]++;
+                }
+                else if (value == 80)
+                {
+                    steel.Dequeue();
+                    carbon.Pop();
+                    if (!forgedSwords.ContainsKey("Shamshir"))
+                    {
+                        forgedSwords.Add("Shamshir", 0);
+                    }
+
+                    forgedSwords["Shamshir"]++;
+                }
+                else if (value == 90)
+                {
+                    steel.Dequeue();
+                    carbon.Pop();
+                    if (!forgedSwords.ContainsKey("Katana"))
+                    {
+                        forgedSwords.Add("Katana", 0);
+                    }
+
+                    forgedSwords["Katana"]++;
+                }
+                else if (value == 110)
+                {
+                    steel.Dequeue();
+                    carbon.Pop();
+                    if (!forgedSwords.ContainsKey("Sabre"))
+                    {
+                        forgedSwords.Add("Sabre", 0);
+                    }
+
+                    forgedSwords["Sabre"]++;
+                }
+                else if (value == 150)
+                {
+                    steel.Dequeue();
+                    carbon.Pop();
+                    if (!forgedSwords.ContainsKey("Broadsword"))
+                    {
+                        forgedSwords.Add("Broadsword", 0);
+                    }
+
+                    forgedSwords["Broadsword"]++;
+                }
+                else
+                {
+                    steel.Dequeue();
+                    carbon.Push(carbon.Pop() + 5);
+                }
+            }
+
+            if (forgedSwords.Any())
+            {
+                Console.WriteLine($"You have forged {forgedSwords.Values.Sum()} swords.");
+                forgedSwords = forgedSwords.OrderBy(x => x.Key).ToDictionary(x => x.Key, x=> x.Value);
+            }
+            else
+            {
+                Console.WriteLine("You did not have any resources to forge a sword.");
+            }
+
+            if (steel.Any())
+            {
+                Console.WriteLine($"Steel left: {string.Join(", ", steel)}");
+            }
+            else
+            {
+                Console.WriteLine("Steel left: none");
+            }
+
+            if (carbon.Any())
+            {
+                Console.WriteLine($"Carbon left: {string.Join(", ", carbon)}");
+            }
+            else
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+
+            if (forgedSwords.Any())
+            {
+                foreach (var forgedSwordsKey in forgedSwords)
+                {
+                    Console.WriteLine($"{forgedSwordsKey.Key}: {forgedSwordsKey.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ 02. Armory/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _02._Armory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] armory = new Char[size, size];
+
+            int or = 0;
+            int oc = 0;
+            int swordsBought = 0;
+            int index = 0;
+
+            string[] coordinates = new string[2];
+
+            for (int i = 0; i < armory.GetLength(0); i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < armory.GetLength(1); j++)
+                {
+                    armory[i, j] = input[j];
+
+                    if (armory[i,j] == 'A')
+                    {
+                        or = i;
+                        oc = j;
+                    }
+
+                    if (armory[i,j] == 'M')
+                    {
+                        coordinates[index] = $"{i} {j}";
+                        index++;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string movement = Console.ReadLine();
+
+                armory[or, oc] = '-';
+
+                if (movement == "up")
+                {
+                    or--;
+                }
+                else if (movement == "down")
+                {
+                    or++;
+                }
+                else if (movement == "left")
+                {
+                    oc--;
+                }
+                else
+                {
+                    oc++;
+                }
+
+                if (or >= 0 && or < armory.GetLength(0) && oc >= 0 && oc < armory.GetLength(1))
+                {
+                    if (armory[or,oc] >= 49 && armory[or,oc] <= 57)
+                    {
+                        swordsBought += int.Parse(armory[or, oc].ToString());
+                    }
+                    else if (armory[oc,or] == 'M')
+                    {
+                        int[] zero = coordinates[0].Split(" ").Select(int.Parse).ToArray();
+                        int[] one = coordinates[1].Split(" ").Select(int.Parse).ToArray();
+
+                        if (zero[0] == or && zero[1] == oc)
+                        {
+                            armory[or, oc] = '-';
+
+                            or = one[0];
+                            oc = one[1];
+                        }
+                        else
+                        {
+                            armory[or, oc] = '-';
+
+                            or = zero[0];
+                            oc = zero[1];
+                        }
+                    }
+
+                    armory[or, oc] = 'A';
+
+                    if (swordsBought >= 65)
+                    {
+                        Console.WriteLine("Very nice swords, I will come back for more!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I do not need more swords!");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"The king paid {swordsBought} gold coins.");
+            for (int i = 0; i < armory.GetLength(0); i++)
+            {
+                for (int j = 0; j < armory.GetLength(1); j++)
+                {
+                    Console.Write(armory[i,j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
