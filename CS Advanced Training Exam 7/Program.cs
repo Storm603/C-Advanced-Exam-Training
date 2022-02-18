a605ae31b181857fa6e70b607c7b844342d498d6
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-02-18 23:22:04 +0200

    Add files via upload

---
+++ CS Advanced Training Exam 7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CS_Advanced_Training_Exam_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstLootBox = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> secondLootBox = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int claimedItemsSum = 0;
+
+            while (firstLootBox.Any() && secondLootBox.Any())
+            {
+                if ((firstLootBox.Peek() + secondLootBox.Peek()) % 2 == 0)
+                {
+                    claimedItemsSum += firstLootBox.Dequeue() + secondLootBox.Pop();
+                }
+                else
+                {
+                    firstLootBox.Enqueue(secondLootBox.Pop());
+                }
+            }
+
+            if (firstLootBox.Any())
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine($"First lootbox is empty");
+            }
+
+            if (claimedItemsSum >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {claimedItemsSum}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {claimedItemsSum}");
+            }
+        }
+    }
+}
