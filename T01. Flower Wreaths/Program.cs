0bed123d19d60d857e46672fdba87b94918e0ca1
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-02-20 00:37:32 +0200

    Add files via upload

---
+++ T01. Flower Wreaths/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01._Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int storedFlowers = 0;
+            int wreaths = 0;
+
+            while (roses.Count > 0 && lilies.Count > 0)
+            {
+                if (roses.Peek() + lilies.Peek() == 15)
+                {
+                    wreaths++;
+                    roses.Dequeue();
+                    lilies.Pop();
+                }
+                else if (roses.Peek() + lilies.Peek() > 15)
+                {
+                    lilies.Push(lilies.Pop() - 2);
+                }
+                else
+                {
+                    storedFlowers += roses.Dequeue();
+                    storedFlowers += lilies.Pop();
+                }
+            }
+
+            int final = storedFlowers / 15;
+            wreaths += final;
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+        }
+    }
+}
---
+++ T02. Bee/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace T02._Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimentions = int.Parse(Console.ReadLine());
+
+            char[,] territory = new char[dimentions, dimentions];
+
+            int br = 0;
+            int bc = 0;
+
+            for (int i = 0; i < territory.GetLength(0); i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < territory.GetLength(1); j++)
+                {
+                    territory[i, j] = input[j];
+                    if (territory[i,j] == 'B')
+                    {
+                        br = i;
+                        bc = j;
+                    }
+                }
+            }
+
+            int polinatedFlowers = 0;
+            string movement = Console.ReadLine();
+
+            while (movement != "End")
+            {
+                territory[br, bc] = '.';
+
+                if (movement == "up")
+                {
+                    br--;
+                    if (!IsInRange(territory, br, bc))
+                    {
+                        break;
+                    }
+                    if (territory[br,bc] == 'O')
+                    {
+                        territory[br, bc] = '.';
+
+                        br--;
+                    }
+                }
+                else if (movement == "down")
+                {
+                    br++;
+                    if (!IsInRange(territory, br, bc))
+                    {
+                        break;
+                    }
+                    if (territory[br, bc] == 'O')
+                    {
+                        territory[br, bc] = '.';
+
+                        br++;
+                    }
+                }
+                else if (movement == "left")
+                {
+                    bc--;
+                    if (!IsInRange(territory, br, bc))
+                    {
+                        break;
+                    }
+                    if (territory[br, bc] == 'O')
+                    {
+                        territory[br, bc] = '.';
+
+                        bc--;
+                    }
+                }
+                else if (movement == "right")
+                {
+                    bc++;
+                    if (!IsInRange(territory, br, bc))
+                    {
+                        break;
+                    }
+                    if (territory[br, bc] == 'O')
+                    {
+                        territory[br, bc] = '.';
+
+                        bc++;
+                    }
+                }
+
+                if (!IsInRange(territory, br, bc))
+                {
+                    break;
+                }
+
+                if (territory[br,bc] == 'f')
+                {
+                    polinatedFlowers++;
+                }
+
+                territory[br, bc] = 'B';
+
+                movement = Console.ReadLine();
+            }
+
+            if (!IsInRange(territory, br, bc))
+            {
+                Console.WriteLine("The bee got lost!");
+            }
+
+            if (polinatedFlowers < 5)
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - polinatedFlowers} flowers more");
+            }
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to polinate {polinatedFlowers} flowers!");
+            }
+
+            for (int i = 0; i < territory.GetLength(0); i++)
+            {
+                for (int j = 0; j < territory.GetLength(1); j++)
+                {
+                    Console.Write(territory[i,j]);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        public static bool IsInRange(char[,] territory, int br, int bc)
+        {
+            return br >= 0 && br < territory.GetLength(0) && bc >= 0 && bc < territory.GetLength(1);
+        }
+    }
+}
